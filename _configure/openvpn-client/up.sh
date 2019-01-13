b8c4b9536b49f84fe9f56d8af1e00ee5ccd7654e
Vyacheslav Voronenko <git@voronenko.info>
2019-01-13 18:36:06 +0100

    better up config

--- _configure/openvpn-client/up.sh
+++ _configure/openvpn-client/up.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# tap0 | 1500 | 1573 | 192.168.1.246 | 255.255.255.0 | init
+echo "$1 | $2 | $3 | $4 | $5 | $6"
+
+
 if ip link show tap0 | grep "master br0" ; then
     echo "Tap0 is already part of the bridge, skipping"
 else
     echo "Adding Tap0 to bridge"
-    ip link set tap0 master br0    
+    ifconfig $1 0.0.0.0 promisc up
+    ip link set tap0 master br0
 fi
+
