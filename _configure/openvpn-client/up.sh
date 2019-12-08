49f47003ca99a628bdc18ffb7f94f3326336aea3
Vyacheslav <git@voronenko.info>
2019-12-08 17:12:51 +0100

    Update up.sh

--- _configure/openvpn-client/up.sh
+++ _configure/openvpn-client/up.sh
@@ -3,6 +3,7 @@
 # tap0 | 1500 | 1573 | 192.168.1.246 | 255.255.255.0 | init
 echo "$1 | $2 | $3 | $4 | $5 | $6"
 
+OBTAINED_IP=$4
 
 if ip link show tap0 | grep "master br0" ; then
     echo "Tap0 is already part of the bridge, skipping"
